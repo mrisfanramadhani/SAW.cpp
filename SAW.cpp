6ea7f1dcc5d4e619b0476db3f7398d48cecf3cbc
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2018-01-02 12:08:20 +0800

    Hasil Akhir

--- SAW.cpp
+++ SAW.cpp
@@ -45,4 +45,13 @@ int main(){
 		}cout <<endl;
 	}
 	cout << endl;
+
+	//Hasil Akhir
+	cout << "HASIL AKHIR :"<<endl;
+	for(int i=0;i <3;i ++){
+		cout<<"Hasil "<<i+1<<" = "<<RWa[i][1]+RWa[i][2]+RWa[i][3]+RWa[i][4]<<endl;
+	}
+	cout <<endl;
+}
+
 	
